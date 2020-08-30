d83aa8d690453f253b5a70e8f457670f3f162b31
rainy-me <github@rainy.me>
2020-08-30 20:40:06 +0900

    restore to lasy 7 days

--- dist/index.js
+++ dist/index.js
@@ -846,7 +846,7 @@ module.exports = (function(e, t) {
     const p = new n(u);
     const c = new s({ auth: `token ${a}` });
     async function main() {
-      const e = await p.getMyStats({ range: i.LAST_YEAR });
+      const e = await p.getMyStats({ range: i.LAST_7_DAYS });
       await updateGist(e);
     }
     async function updateGist(e) {
